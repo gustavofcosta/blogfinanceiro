e997714e5eb04a18971fb4570d1164cf035b431e
Gustavo Fernandes Costa <84602100+gustavofcosta@users.noreply.github.com>
2022-04-15 21:51:22 -0300

    commit

--- blog/components/UserWindow.tsx
+++ blog/components/UserWindow.tsx
@@ -17,10 +17,11 @@ export default function useWindowDimensions() {
     const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
 
     useEffect(() => {
+        function handleResize() {
+            setWindowDimensions(getWindowDimensions());
+        }
         if (hasWindow) {
-            function handleResize() {
-                setWindowDimensions(getWindowDimensions());
-            }
+
 
             window.addEventListener('resize', handleResize);
             return () => window.removeEventListener('resize', handleResize);
