7cf775b62a7eec95c318a833639d8946526bc31e
Gustavo Fernandes Costa <84602100+gustavofcosta@users.noreply.github.com>
2022-04-20 07:14:54 -0300

    Update index.tsx

--- blog/pages/index.tsx
+++ blog/pages/index.tsx
@@ -94,12 +94,11 @@ export default function Home({ posts, filteredCoins, noticias, title, url }: Pro
                       <p className='text-xs'>{noticia.description} por {noticia.author.name}</p>
                     </div>
                      
-                    <Image
+                    <img
                       className='h-12 w-12 rounded-full'
                       src={urlFor(noticia.author.image).url()!} 
                       alt={noticia.author.name}
-                      width={48}
-                      height={48}
+                     
                     />
                    
                   </div>
@@ -131,12 +130,11 @@ export default function Home({ posts, filteredCoins, noticias, title, url }: Pro
                       <p className='text-xs'>{noticia.description} por {noticia.author.name}</p>
                     </div>
                    
-                    <Image
+                    <img
                       className='h-12 w-12 rounded-full'
                       src={urlFor(noticia.author.image).url()!} 
                       alt={noticia.author.name}
-                      width={48}
-                      height={48}
+                      
                     />
                    
                   </div>
@@ -169,12 +167,11 @@ export default function Home({ posts, filteredCoins, noticias, title, url }: Pro
                       <p className='text-xs'>{post.description} por {post.author.name}</p>
                     </div>
                
-                    <Image
+                    <img
                       className='h-12 w-12 rounded-full'
                       src={urlFor(post.author.image).url()!} 
                       alt={post.author.name}
-                      width={48}
-                      height={48}
+                      
                     />
              
                   </div>
