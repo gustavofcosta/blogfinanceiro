8c697932dfeaaf725f975fd9400f623bf4654ec3
Gustavo Fernandes Costa <84602100+gustavofcosta@users.noreply.github.com>
2022-05-03 04:05:01 -0300

    Update [slug].tsx

--- blog/pages/post/[slug].tsx
+++ blog/pages/post/[slug].tsx
@@ -257,7 +257,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
         slug: params?.slug,
     });
 
-    const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&order=market_cap_desc&per_page=250&page=1&sparkline=false')
+    const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&order=market_cap_desc&per_page=30&page=1&sparkline=false')
 
     const filteredCoins = await res.json()
 
